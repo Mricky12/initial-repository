4e0cb03d5050baf2489d7d8066f6fbe332273b54
SewawasE <chikki.chicken1011@gmail.com>
2024-12-26 15:21:25 +0900

    edituser.jsp

--- src/main/java/task/dto/UsersDTO.java
+++ src/main/java/task/dto/UsersDTO.java
@@ -3,60 +3,61 @@ package task.dto;
 import java.time.LocalDateTime;
 
 public class UsersDTO {
-    private int id; // ユーザーID
-    private String name; // ユーザー名
-    private String email; // メールアドレス
-    private String password; // パスワード
-    private LocalDateTime deletedAt; // ソフトデリート用
+	private int id; // ユーザーID
+	private String name; // ユーザー名
+	private String email; // メールアドレス
+	private String password; // パスワード
+	private LocalDateTime deletedAt; // ソフトデリート用
 
-    public UsersDTO() {}
+	public UsersDTO() {
+	}
 
-    public UsersDTO(int id, String name, String email, String password, LocalDateTime deletedAt) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-        this.password = password;
-        this.deletedAt = deletedAt;
-    }
+	public UsersDTO(int id, String name, String email, String password, LocalDateTime deletedAt) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		this.deletedAt = deletedAt;
+	}
 
-    // Getter and Setter
-    public int getId() {
-        return id;
-    }
+	// Getter and Setter
+	public int getId() {
+		return id;
+	}
 
-    public void setId(int id) {
-        this.id = id;
-    }
+	public void setId(int id) {
+		this.id = id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    public String getPassword() {
-        return password;
-    }
+	public String getPassword() {
+		return password;
+	}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+	public void setPassword(String password) {
+		this.password = password;
+	}
 
-    public LocalDateTime getDeletedAt() {
-        return deletedAt;
-    }
+	public LocalDateTime getDeletedAt() {
+		return deletedAt;
+	}
 
-    public void setDeletedAt(LocalDateTime deletedAt) {
-        this.deletedAt = deletedAt;
-    }
+	public void setDeletedAt(LocalDateTime deletedAt) {
+		this.deletedAt = deletedAt;
+	}
 }
